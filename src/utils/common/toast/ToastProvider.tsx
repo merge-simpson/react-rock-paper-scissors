c87a5d2752401b6ce23fd691209f7ba2aeacdcee
merge-simpson <letsdev@kakao.com>
2022-11-02 06:18:38 +0900

    fix(toast): add z-[999] on toast

--- src/utils/common/toast/ToastProvider.tsx
+++ src/utils/common/toast/ToastProvider.tsx
@@ -14,7 +14,7 @@ const ToastProvider: FC<BrowserToastProps> = (props) => {
     <div>
       {toast.isOpen && (
         <motion.div
-          className="fixed top-3/4 -translate-y-1/2 left-1/2 -translate-x-1/2 min-w-[50vw] bg-black bg-opacity-70 text-white rounded px-2 py-1 text-center"
+          className="fixed z-[999] top-3/4 -translate-y-1/2 left-1/2 -translate-x-1/2 min-w-[50vw] bg-black bg-opacity-70 text-white rounded px-2 py-1 text-center"
           initial={{ opacity: 0.5 }}
           animate={{ opacity: 1.0 }}
           transition={{ ease: "easeInOut", duration: 0.3 }}
